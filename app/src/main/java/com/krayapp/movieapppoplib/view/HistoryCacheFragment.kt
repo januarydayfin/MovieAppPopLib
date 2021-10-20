fa47463a04fd7590489e464e03690d088bdd3e86
krayman <50882897+krayman@users.noreply.github.com>
2021-10-20 13:35:46 +0700

    Нарисовал тулбарь

--- app/src/main/java/com/krayapp/movieapppoplib/view/HistoryCacheFragment.kt
+++ app/src/main/java/com/krayapp/movieapppoplib/view/HistoryCacheFragment.kt
@@ -5,7 +5,10 @@ import com.krayapp.movieapppoplib.R.layout.history_cache_fragment
 import com.krayapp.movieapppoplib.view.mvpViews.HistoryCacheView
 
 
-class HistoryCacheFragment:AbsFragment(history_cache_fragment), HistoryCacheView {
+class HistoryCacheFragment : AbsFragment(history_cache_fragment), HistoryCacheView {
+    companion object {
+        fun newInstance() = HistoryCacheFragment()
+    }
 
     override fun showCache() {
         TODO("Not yet implemented")
--- app/src/main/java/com/krayapp/movieapppoplib/view/Screens/HistoryCacheScreen.kt
+++ app/src/main/java/com/krayapp/movieapppoplib/view/Screens/HistoryCacheScreen.kt
@@ -1,4 +1,11 @@
 package com.krayapp.movieapppoplib.view.Screens
 
-class HistoryCacheScreen {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
+import com.github.terrakok.cicerone.androidx.FragmentScreen
+import com.krayapp.movieapppoplib.view.HistoryCacheFragment
+
+object HistoryCacheScreen:FragmentScreen {
+    override fun createFragment(factory: FragmentFactory): Fragment =
+        HistoryCacheFragment.newInstance()
 }
